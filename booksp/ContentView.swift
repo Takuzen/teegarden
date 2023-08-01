1c1f7f356c4dea0a5da43acdcb72e4ab089146d2
Takuya Matsuda <hilcrhyme.t.toc.katsu@gmail.com>
2023-08-01 23:50:32 +0900

    impl: Viewmodel for firebase. - You need to add GoogleService-Info.plist and firebase-ios-sdk - Have not tested firestore yet. - For visionOS, you need to care about firebase-ios-sdk version. - Maybe you need to add keychain-access.

--- booksp/ContentView.swift
+++ booksp/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 import RealityKit
 import RealityKitContent
 
@@ -16,12 +17,21 @@ struct ContentView: View {
     @Environment(\.openImmersiveSpace) var openImmersiveSpace
     @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
 
+    @ObservedObject var viewModel = FirebaseViewModel()
+    
     var body: some View {
         NavigationSplitView {
             List {
                 Text("Item")
             }
             .navigationTitle("Sidebar")
+            
+            if viewModel.isUserLoggedIn() {
+                loggedInView
+            } else {
+                loggedOutView
+            }
+            
         } detail: {
             VStack {
                 Model3D(named: "Scene", bundle: realityKitContentBundle)
@@ -46,6 +56,60 @@ struct ContentView: View {
             }
         }
     }
+    
+    private var loggedInView: some View {
+        VStack {
+            Text("Your favorite books:")
+            
+            List(viewModel.favoriteBooks) { book in
+                Text("\(book.title) by \(book.author) (\(book.year))")
+            }
+            
+            Text(viewModel.errorMessage)
+            
+            Button(action: {
+                // Log out when button is pressed
+                viewModel.signOut()
+            }) {
+                Text("Sign Out")
+            }
+        }
+    }
+    
+    private var loggedOutView: some View {
+        VStack {
+            TextField("Email", text: $viewModel.mail)
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+            
+            SecureField("Password", text: $viewModel.password)
+            
+            Text(viewModel.errorMessage)
+            
+//            TextField("メールアドレスを入力してください",text: $mail)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding()
+//                
+//            SecureField("パスワードを入力してください",text:$password)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding()
+        
+            Button(action: {
+                viewModel.signUp()
+            }) {
+                Text("Sign Up")
+            }
+            
+            Button(action: {
+                // Sign in when button is pressed
+                viewModel.login()
+            }) {
+                Text("Log In")
+            }
+            
+        }
+    }
+
 }
 
 #Preview {
