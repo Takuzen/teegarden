bfa7ea5ed23b052f3e80d3a6b189c1064d597020
Takuzen Toh <takuzen0430@gmail.com>
2024-01-16 09:29:03 +0900

    chore: RealityView

--- booksp/CubeModelView.swift
+++ booksp/CubeModelView.swift
@@ -7,14 +7,28 @@
 
 import SwiftUI
 import RealityKit
+import RealityKitContent
 
 struct CubeModelView: View {
     private let sample_3dmodelurl = URL(string: "https://developer.apple.com/augmented-reality/quick-look/models/teapot/teapot.usdz")!
     
+    /// The sun entity that the view creates and stores for later updates.
+    @State private var sample_model: Entity?
+    
     var body: some View {
         RealityView { content in
-            let url = URL(fileURLWithPath: "/Users/ttoh/booksp/Packages/RealityKitContent/Sources/RealityKitContent/RealityKitContent.rkassets/solar_panels.usdz")
-            let entity = try? Entity.load(contentsOf: url)
+            guard let sample_model = await RealityKitContent.entity(named: "solar_panels") else {
+                return
+            }
+            
+            content.add(sample_model)
+            self.sample_model = sample_model
         }
     }
 }
+            /*
+             RealityView { content in
+             let url = URL(fileURLWithPath: "/Users/ttoh/booksp/Packages/RealityKitContent/Sources/RealityKitContent/RealityKitContent.rkassets/solar_panels.usdz")
+             let entity = try? Entity.load(contentsOf: url)
+             }
+             */
