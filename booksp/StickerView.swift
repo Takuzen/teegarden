22d570c8409acda603bbafa296f5f677463ffe99
Takuzen Toh <takuzen0430@gmail.com>
2024-03-21 22:11:05 -0700

    feat: sticker not snapshot but vstack can input text

--- booksp/StickerView.swift
+++ booksp/StickerView.swift
@@ -10,39 +10,26 @@ import SwiftUI
 struct StickerView: View, Identifiable {
     
     @State private var stickerText = ""
+    @State private var isEditing: Bool = false
 
     var viewModel = ViewModel()
     
     var id = UUID()
 
     var body: some View {
-        viewToSnapshot()
-    }
-
-    func viewToSnapshot() -> some View {
         VStack {
-            GeometryReader { geometry in
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(Color.yellow.opacity(0.6))
-                    .overlay(
-                        VStack {
-                            TextEditor(text: $stickerText)
-                            /*
-                            Text("X: \(geometry.frame(in: .global).origin.x) Y: \(geometry.frame(in: .global).origin.y) width: \(geometry.frame(in: .global).width) height: \(geometry.frame(in: .global).height)")
-                                .foregroundColor(.white)
-                             */
-                        }
-                    )
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.yellow.opacity(0.6))
+                .overlay(
+                    VStack {
+                        TextEditor(text: $stickerText)
+                        /*
+                        Text("X: \(geometry.frame(in: .global).origin.x) Y: \(geometry.frame(in: .global).origin.y) width: \(geometry.frame(in: .global).width) height: \(geometry.frame(in: .global).height)")
+                            .foregroundColor(.white)
+                         */
+                    }
+                )
             }
-        }
         .frame(width: targetSize.width, height: targetSize.height)
-    }
-
-    @MainActor
-    func generateSnapshot() async {
-        let renderer = ImageRenderer(content: viewToSnapshot())
-        if let image = renderer.uiImage {
-            viewModel.targetSnapshot = image
-        }
     }
 }
